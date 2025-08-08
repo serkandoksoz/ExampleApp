f7a5c1d715d364cb12ad3a477a04f1d1b30987f7
Serkan Doksoz <serkan.doksoz@elekse.com>
2025-08-08 15:17:48 +0300

    Initial Commit

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Serkan Doksöz on 8.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
